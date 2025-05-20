0a6d35f45e2342b02967ba529b64ea41b5a45d73
RockyKit <99561580+RayMidBass@users.noreply.github.com>
2025-05-19 23:23:28 -0600

    Create Actividades.jsx

---
+++ Actividades.jsx
@@ -0,0 +1,9 @@
+// src/pages/Actividades.jsx
+const Actividades = () => (
+  <div className="p-4">
+    <h1 className="text-xl font-bold">Actividades del Curso</h1>
+    <p>Resumen de los sprints realizados, tareas, y aprendizajes.</p>
+  </div>
+);
+
+export default Actividades;
