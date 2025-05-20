1ff03e2246c666aea523f859369c947892de1261
RockyKit <99561580+RayMidBass@users.noreply.github.com>
2025-05-19 23:23:45 -0600

    Create Sistema.jsx

---
+++ Sistema.jsx
@@ -0,0 +1,9 @@
+// src/pages/Sistema.jsx
+const Sistema = () => (
+  <div className="p-4">
+    <h1 className="text-xl font-bold">Sistema Implementado</h1>
+    <p>Descripción del sistema desarrollado, objetivos y funcionalidades.</p>
+  </div>
+);
+
+export default Sistema;
